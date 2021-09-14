74e67a2af0ebaf9d5f9aa6e1bd49f5c571802766
tangyao <tangy@lamic.cn>
2021-09-14 18:56:48 +0800

    本地调用

--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -10,18 +10,41 @@
 </template>
 <script>
 import { mapState } from 'vuex'
+const exec = require('child_process').exec
 export default {
   data() {
     return {
       backgroundImage: require("./../assets/Images/splashscreen.png"),
     };
   },
-
   mounted() {
     this.initSystem();
   },
   methods: {
     initSystem() {
+      let self = this;
+      //启动通讯进程
+      //let filePath = 'D:\\chagoi_native-master\\chagoi.native.core\\bin\\Debug\netcoreapp3.1\\chagoi.native.core.exe';
+      let cmdStr = "dotnet D:\\chagoi_native-master\\chagoi.native.core\\bin\\Debug\\netcoreapp3.1\\chagoi.native.core.dll";
+      let workerProcess = exec(cmdStr)
+      // 打印正常的后台可执行程序输出
+      workerProcess.stdout.on('data', function (data) {
+        console.log('stdout: ' + data)
+      })
+      // 打印错误的后台可执行程序输出
+      workerProcess.stderr.on('data', function (data) {
+        console.log('stderr: ' + data)
+      })
+      // 退出之后的输出
+      workerProcess.on('close', function (code) {
+        if (code != 0) {
+          self.$alert('抱歉,启动通讯进程失败', '通讯失败', {
+            confirmButtonText: '确定'
+          })
+          return
+        }
+        this.$store.commit('native_ws/setIsRuning', true)
+      })
     }
   }, computed: {
     ...mapState({
