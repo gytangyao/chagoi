15aa499f12950bff64d620da9a2d4df6572a37d7
tangyao <tangy@lamic.cn>
2021-09-16 19:00:24 +0800

    调整了MessageBox样式

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -64,6 +64,7 @@
       </el-button>
     </div>
   </div>
+
 </template>
 
 <script>
@@ -276,7 +277,24 @@ export default {
       });
     },
     exitApp() {
-      alert(1);
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+        customClass: "MyMsgBox"
+      })
+        .then(() => {
+          this.$message({
+            type: 'success',
+            message: '删除成功!',
+          })
+        })
+        .catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除',
+          })
+        })
     },
   },
   components: {
